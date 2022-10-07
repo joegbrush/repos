e3c302d6ae38b4ac944b5bc912bf2ec08a35f9f5
Joe Brush <joegbrush@gmail.com>
2022-10-07 15:45:39 -0500

    remove old repositories

--- myExcercises/app.js
+++
@@ -1,9 +0,0 @@
-// myExcercises
-const personFactory = (name, age) => {
-  const sayHello = () => console.log('hello!');
-  return { name, age, sayHello };
-};
-
-const sam = personFactory('Sam', 13);
-
-console.log(sam.sayHello);
