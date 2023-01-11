3948b0f629600b7d1dcc8d9f668287b2df46f67e
Joe Brush <joegbrush@gmail.com>
2023-01-11 11:01:52 -0600

    jan 11

--- delete/delete.js
+++ delete/delete.js
@@ -1,6 +1,20 @@
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  const fuel = fuelLeft * mpg;
-  return distanceToPump >= fuel;
-};
+function getDrinkByProfession(param){
+	switch (param) {
+		case "Jabroni":
+		return	"Patron Tequila";
+		case "School Counselor":
+		return	"Anything with Alcohol";
+		case "Programmer":
+		return	"Hipster Craft Beer";
+		case "Bike Gang Member":
+		return	"Moonshine";
+		case "Politician":
+		return	"Your tax dollars";
+		case "Rapper":
+		return	"Cristal";
+		default:
+		return "Beer";
+	}
+}
 
-console.log(zeroFuel(50, 25, 2));
+console.log(getDrinkByProfession("Politician"))
